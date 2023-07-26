c0147dc524e838cec1222105a2cea788ea3dc623
Mindp1 <patel.manan@gmail.com>
2023-07-26 15:38:10 -0500

    Introduce enum to make date array usage clearer
    An enum has been introduced in the main.cpp file to make the usage of date array more understandable. Earlier, the date array was directly being accessed using indices such as 0, 1, 2 which was hard to understand. Now, these indices have been replaced by 'month', 'day', 'year' respectively, which makes the code easier to comprehend. Also, the calculation of 'months_here' variable and 'levelProgress' variable now includes the current month. This change enhances the readability and maintainability of the code.

--- main.cpp
+++ main.cpp
@@ -5,14 +5,20 @@
 #include <utility>
 using std::cout, std::cin, std::string;
 
+enum index {
+    month,
+    day,
+    year
+};
+
 void calculateMonthsHere(int startDate[], int endDate[], int &months_here);
 
 int main() {
     int currentDate[3]; //= {6, 21, 2023};
     char sep;
     cout << "Enter the current date\n";
-    cin >> currentDate[0] >> sep >> currentDate[1] >> sep
-        >> currentDate[2];
+    cin >> currentDate[month] >> sep >> currentDate[day] >> sep
+        >> currentDate[year];
     bool anotherStudent = true;
     while (anotherStudent) {
         int startDate[3]; // month, day, year
@@ -32,65 +38,68 @@ int main() {
             if (hasMultipleEnrollments) {
                 int endDate[3];
                 cout << "Enter start date\n";
-                cin >> startDate[0] >> sep >> startDate[1] >> sep
-                    >> startDate[2];
+                cin >> startDate[month] >> sep >> startDate[day] >> sep
+                    >> startDate[year];
                 cout << "Enter days on hold\n";
                 cin >> daysOnHold;
-                if (startDate[1] > 6) {
-                    ++startDate[0];
+                if (startDate[day] > 6) {
+                    ++startDate[month];
                 }
                 cout << "Enter end date\n";
-                cin >> endDate[0] >> sep >> endDate[1] >> sep
-                    >> endDate[2];
+                cin >> endDate[month] >> sep >> endDate[day] >> sep
+                    >> endDate[year];
                 calculateMonthsHere(startDate, endDate, months_here);
+                months_here -= daysOnHold / 30;
                 cout << "Another enrollment with an end date?(Yy/Nn)\n";
                 cin >> yesNo;
                 repeat = tolower(yesNo) == 'y';
             }
         }
         cout << "Enter start date\n";
-        cin >> startDate[0] >> sep >> startDate[1] >> sep >> startDate[2];
+        cin >> startDate[month] >> sep >> startDate[day] >> sep >>
+        startDate[year];
         cout << "Enter days on hold\n";
         cin >> daysOnHold;
-        if (startDate[1] > 6) {
-            ++startDate[0];
+        if (startDate[day] > 6) {
+            ++startDate[month];
         }
         calculateMonthsHere(startDate, currentDate, months_here);
+        ++months_here; //Includes this month
         months_here -= daysOnHold / 30;
         int currentLevel = months_here / 6 + 1;
         if (currentLevel > 5) {
             currentLevel = 5;
         }
-        int levelProgress = months_here % 6;
+        int levelProgress = months_here % 6; //Includes this month
         for (int i = 0; i < 6 - levelProgress; ++i) {
-            if (startDate[0] == 12) {
-                startDate[0] = 1;
-                ++startDate[2];
+            if (startDate[month] == 12) {
+                startDate[month] = 1;
+                ++startDate[year];
             } else {
-                ++startDate[0];
+                ++startDate[month];
             }
         }
-        if (startDate[0] == 12) {
-            startDate[0] = 1;
-            ++startDate[2];
+        if (startDate[month] == 12) {
+            startDate[month] = 1;
+            ++startDate[year];
         } else {
-            ++startDate[0];
+            ++startDate[month];
         }
         cout << "Months here: " << months_here << '\n'
              << "Current Level: " << currentLevel << '\n'
              << "Months toward next level: " << months_here % 6 << '\n';
         cout << "Date for level " << currentLevel + 1 << ": "
-             << startDate[0] << '/' << startDate[2] << '\n';
+             << startDate[month] << '/' << startDate[year] << '\n';
         for (int i = 0; i < 6; ++i) {
-            if (startDate[0] == 12) {
-                startDate[0] = 1;
-                ++startDate[2];
+            if (startDate[month] == 12) {
+                startDate[month] = 1;
+                ++startDate[year];
             } else {
-                ++startDate[0];
+                ++startDate[month];
             }
         }
         cout << "Date for level " << currentLevel + 2 << ": "
-             << startDate[0] << '/' << startDate[2] << '\n';
+             << startDate[month] << '/' << startDate[year] << '\n';
         cout << "Would you like to enter another students information?"
                 "(Yy/Nn)\n";
         cin >> yesNo;
@@ -100,26 +109,27 @@ int main() {
 
 void calculateMonthsHere(int startDate[], int endDate[],
                          int &months_here) {
-    if ((endDate[0] == 2 && endDate[1] == 28)
-        || endDate[1] == 30 || endDate[1] == 31) {
-        ++endDate[0];
+    if ((endDate[month] == 2 && endDate[day] == 28)
+        || endDate[day] == 30 || endDate[day] == 31) {
+        ++endDate[month];
     }
-    while (startDate[2] < endDate[2]) {
+    while (startDate[year] < endDate[year]) {
         ++months_here;
-        if (startDate[0] == 12) {
-            startDate[0] = 1;
-            ++startDate[2];
+        if (startDate[month] == 12) {
+            startDate[month] = 1;
+            ++startDate[year];
         } else {
-            ++startDate[0];
+            ++startDate[month];
         }
     }
-    while (startDate[0] < endDate[0]) {
+    while (startDate[month] < endDate[month]) {
         ++months_here;
-        if (startDate[0] == 12) {
-            startDate[0] = 1;
-            ++startDate[2];
+        if (startDate[month] == 12) {
+            startDate[month] = 1;
+            ++startDate[year];
         } else {
-            ++startDate[0];
+            ++startDate[month];
         }
     }
+//    ++months_here; //Includes this month //NOT right place to put
 }
